8d2862d5fa2b3f5a3c1d415f0d90b089060e7f0b
RRICHARRD <guilherme.rixard@gmail.com>
2021-03-18 21:13:48 -0300

    add commands that change fields and delete table and trucate, this is danger to lost your job, please pay attention

--- 5_insert-in-table-and-showInformations-in-fields.sql
+++ 5_insert-in-table-and-showInformations-in-fields.sql
@@ -48,7 +48,7 @@ insert into clientes values
 select * from clientes; 
 
 -- Alterar informações da tabela 
--- Nunca faça o sequintes código 
+-- Cuidado com os seguintes códigos, PRINCIPALMENTE o da linha 52 
 update clientes set DataNascimento = '1999-12-20'; -- isso apaga a tabela do banco de dados INTEIRA, se fizer isso será demitido.
 update clientes set ufNascimento = 'mg' where ufNascimento is null; -- Não funciona como o da linha 53, devido ao fato do safe update estar ligado, necessitando utilizar uma chave primária
 /*Para desligar o safe mode, Edit -> Preferences -> Sql editor, lá no final, salvar sair da seção, depois entrar novamente*/
@@ -58,3 +58,15 @@ update clientes set ufNascimento = 'mg' where ufNascimento is null; -- Não func
 update clientes set DataNascimento = '8888-7-06' where idCliente=1;
 update clientes set DataNascimento = '8888-7-06', ufNascimento = 'am' where idCliente=1;
 
+-- deletando registros da tabela, não dá para apagar campos específicos, nesse caso tem que atualizar, se for apagar tem que apagar o registro inteiro
+/*Prestar muito mais atenção*/
+delete from clientes where idCliente = 1;
+delete from clientes where nomeCliente = 'Luiz'; -- esse só funciona com o safe update desligado
+delete from clientes where dataNascimento is null; -- apaga tudo que é possui a data de nascimento null
+
+-- Apagar a tabela inteira, demissão
+delete from clientes; -- vai ser feito linha por linha, mas muito rápido
+/*O DELETE FOI FEITO PARA NUNCA SER UTILIZADO, O IDEAL É MARCAR COMO APAGADO*/
+
+-- Apaga a tabela e cria de novo vazio
+truncate table clientes;
